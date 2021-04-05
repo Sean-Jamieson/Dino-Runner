21e9d5edd8d2c8e0f6baf54cabdd4a85c5e5ac57
sean jamieson <sjami273@mtroyal.ca>
2021-04-04 23:07:52 -0600

    finished enable_sound pending tests

--- SOUND.C
+++ SOUND.C
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <osbind.h>
 
-typedef unsigned int UINT8
+typedef unsigned int UINT8;
+typedef unsigned int UINT32;
 
 void write_psg(int reg, UINT8 val);
 UINT8 read_psg(int reg);
@@ -29,11 +30,10 @@ UINT8 read_psg(int reg)
 {
     volatile char *PSG_reg_select = 0xFF8800;
     volatile char *PSG_reg_write = 0xFF8802;
-    long old_ssp = Super(0);   
+    long old_ssp = Super(0);
     *PSG_reg_select = reg;
     return *PSG_reg_write;
     Super(old_ssp);
-
 }
 
 void set_tone(int channel, int tuning)
@@ -70,22 +70,111 @@ void set_volume(int channel, int volume)
         write_psg(0xA, volume);
     }
 }
-
+/*needs condensing*/
 void enable_channel(int channel, int tone_on, int noise_on)
 {
     UINT8 en_psg;
+
     en_psg = read_psg(7);
-    
+
     if (channel == 0)
     {
-        write_psg(7, volume);
+        if (tone_on && noise_on)
+        {
+            en_psg &= 0x9;
+            write_psg(7, en_psg);
+        }
+        else if (!tone_on && !noise_on)
+        {
+            en_psg |= 0x9;
+            write_psg(7, en_psg);
+        }
+        else if (tone_on && !noise_on)
+        {
+            en_psg &= 0x1;
+            write_psg(7, en_psg);
+            en_psg |= 0x8;
+            write_psg(7, en_psg);
+        }
+        else
+        {
+            en_psg |= 0x1;
+            write_psg(7, en_psg);
+            en_psg &= 0x8;
+            write_psg(7, en_psg);
+        }
     }
     else if (channel == 1)
     {
-        write_psg(9, volume);
+        if (tone_on && noise_on)
+        {
+            en_psg &= 0x12;
+            write_psg(7, en_psg);
+        }
+        else if (!tone_on && !noise_on)
+        {
+            en_psg |= 0x12;
+            write_psg(7, en_psg);
+        }
+        else if (tone_on && !noise_on)
+        {
+            en_psg &= 0x2;
+            write_psg(7, en_psg);
+            en_psg |= 0x10;
+            write_psg(7, en_psg);
+        }
+        else
+        {
+            en_psg |= 0x2;
+            write_psg(7, en_psg);
+            en_psg &= 0x10;
+            write_psg(7, en_psg);
+        }
     }
     else
     {
-        write_psg(0xA, volume);
+        if (tone_on && noise_on)
+        {
+            en_psg &= 0x24;
+            write_psg(7, en_psg);
+        }
+        else if (!tone_on && !noise_on)
+        {
+            en_psg |= 0x24;
+            write_psg(7, en_psg);
+        }
+        else if (tone_on && !noise_on)
+        {
+            en_psg &= 0x4;
+            write_psg(7, en_psg);
+            en_psg |= 0x20;
+            write_psg(7, en_psg);
+        }
+        else
+        {
+            en_psg |= 0x4;
+            write_psg(7, en_psg);
+            en_psg &= 0x20;
+            write_psg(7, en_psg);
+        }
     }
+}
+
+void stop_sound()
+{
+    write_psg(7, 255);
+}
+
+void update_music(UINT32 time_elapsed)
+{
+
+}
+
+void set_noise(int tuning)
+{
+
+}
+
+void set_envelope(int shape, unsigned int sustain)
+{
 }
